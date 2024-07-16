5e5242d5b2dd101460c4a0507cf84cb901896f9f
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-07-16 20:01:45 +0300

    feat: added mechanics of selection of items

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,15 +7,26 @@ public class AnimatorController : MonoBehaviour
     private static readonly int Speed = Animator.StringToHash("speed");
     private Animator _animator;
     private NavMeshAgent _navMeshAgent;
+    private Outline _outline;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _outline = GetComponent<Outline>();
     }
 
     private void Update()
     {
         _animator.SetFloat(Speed, _navMeshAgent.velocity.magnitude);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name=="Potion")
+        {
+            Destroy(other.gameObject);
+            _outline.OutlineWidth = 2;
+        }
     }
 }
